0b641902f76b6947f342bc5c54e743f1a57f7e30
khannnn88 <35554963+khannnn88@users.noreply.github.com>
2023-08-30 18:44:46 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# weatherreact-app
+ 
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+import Search from "./Search";
+
+export default function App() {
+  return (
+    <div className="Container">
+      <h1>Weather Search</h1>
+      <Search />
+    </div>
+  );
+}
